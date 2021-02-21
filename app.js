7e91c3fbabc3f1c0a5f0e209badac7b14211be28
Daniel <danielselareid40@gmail.com>
2021-02-21 19:47:32 +1300

    added notificating brrrrrrrr

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { exec } = require("child_process");
+const axios = require("axios");
 let app = require('express')();
 let http = require('http').createServer(app);
 
@@ -9,16 +10,38 @@ app.get('/', (req, res) => {
 app.get('/on', (req, res) => {
   exec("ipmitool -I lanplus -H 192.168.1.210  -U root -P calvin power on");
   sendPageWithStatus(res);
+  sendNotif("attemp to turn power on");
 });
 
 app.get('/off', (req, res) => {
   exec("ipmitool -I lanplus -H 192.168.1.210  -U root -P calvin power soft");
   sendPageWithStatus(res);
+  sendNotif("attemp to turn power off");
 });
 
 http.listen(4949, function(){
     console.log('listening on *:4949');
 });
+
+function sendNotif(message) {
+  const url = "http://home.selareid.com:2828/message?token=AKuVAVper4EVwdG";
+  const bodyFormData = {
+    title: "Node Power Update",
+    message: message,
+    priority: 1,
+  };
+
+  axios({
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    url: url,
+    data: bodyFormData,
+  })
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err.response ? error.response.data : err));
+}
 
 function sendPageWithStatus(res) {
   exec("ipmitool -I lanplus -H 192.168.1.210  -U root -P calvin power status", (error, stdout, stderr) => {
